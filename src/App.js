d570f05fe8c66a317fe30a87d55372f30b685c7d
Nicolò Pagliarani <nico.pagliarani@gmail.com>
2022-03-30 17:52:48 +0200

    SearchBar to fix

--- src/App.js
+++ src/App.js
@@ -5,25 +5,49 @@ import foods from './foods.json';
 import {useState} from 'react';
 import FoodBox from './components/Foodbox'
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [allFoods,setAllFoods]= useState(foods)
   const [displayList, setDisplayList] = useState(foods)
-
+  const [filter, setFilter] = useState("");
+  
+  
   const addFood = (newFood) => {
 
     const updatedFoodList = [...allFoods, newFood];
     setAllFoods(updatedFoodList);
     setDisplayList(updatedFoodList);
+    }
 
-}
+    const filterFoods = (filter) => {
+      setFilter(filter);
+    }
+    const deleteFoods = (foodName) => {
+      const filteredFood = allFoods.filter((food) => {
+        return food.name !== foodName;
+      });
+      setDisplayList(filteredFood);
+      setAllFoods(filteredFood);
+    };
+    
   return (
     <div className="App">
+      <div>
+      <h1>Search your Food</h1>
+      <Search filterFoods={filterFoods} />
+      </div>
+      <div>
+      <h1>Add your Food</h1>
       <AddFoodForm addFood={addFood}/>
+      </div>
+      
+      
+      
       <h1>Food List</h1>
       {allFoods.map((food)=>{
         return(<div>
-          <FoodBox food={food}/>
+          <FoodBox food={food} deleteFood={deleteFoods}/>
          </div>)
       })
     }
